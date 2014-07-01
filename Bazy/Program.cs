04bb5acc03f789f6cf622098fa73e13a7f80ddd1
staddaleko <staddaleko@gmail.com>
2014-07-01 14:23:57 +0200

    jeszcze nic nie działa, ale się łączy bez błędów

--- Bazy/Program.cs
+++ Bazy/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;//dodane na potrzeby połączenia z sql server
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,24 @@ namespace Bazy
     {
         static void Main(string[] args)
         {
-            string connString = @"Data Source=(local)\SQLEXPRESS;Initial Catalog=Ksiazki;Integrated Security=True;";
+            string connString = @"Data Source=(local)\SQLEXPRESS;Initial Catalog=Ksiazki;Integrated Security=True;";//logowanie zintegrowane korzysta z windows user account //'@' musi być, bo są backslashe
+
+            SqlConnection conn = new SqlConnection(connString);
+//piszemy 'starą szkołę', ale pokazuje dokładnie w jaki sposób działa połączenie            
+            try
+            {
+                conn.Open();//otwieramy połączenie
+            }
+            catch (Exception ex)//łapiemy wyjątka ;)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+                conn.Close();//zamykamy połączenie
+            }
+            Console.ReadKey();
         }
     }
 }
