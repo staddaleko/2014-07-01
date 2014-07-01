577307d118de7f0bf9ca3b5dbbb68a5083bbd1db
staddaleko <staddaleko@gmail.com>
2014-07-01 14:49:56 +0200

    kolejne zmiany i wciąż działa!!! ;P

--- Bazy/Program.cs
+++ Bazy/Program.cs
@@ -22,9 +22,17 @@ namespace Bazy
                 comm.CommandText = "Select Count(*) from Ksiazki";
                 comm.CommandType = System.Data.CommandType.Text;//w zasadzie ta komenda nie jest potrzebna, bo w domyśle komendy są wydawane w tekście... natomiast pokazuje możlwiości i składnię...
                 comm.Connection = conn;//pokazuje z którą bazą danych mamy się połączyć.
-
-                int ilosc = (int)comm.ExecuteScalar();
-                Console.WriteLine("Ilość książek: {0}", ilosc);
+                
+                
+                int ilosc = (int)comm.ExecuteScalar();//--powyższe zapytanie zwraca pojedyńczą wartość, więc można tę komendę zastosować--
+                Console.WriteLine("Ilość książek: {0}.", ilosc);
+            }
+            catch (SqlException ex)//łapiemy wyjątka typowo sqlowego
+            {
+                foreach (SqlError item in ex.Errors)//wyszczególniamy wszystkie errory
+                {
+                    Console.WriteLine("Klasa błędu: {0}, \nNumer błędu: {1}, \nOpis błędu: {2}.", item.Class, item.Number, item.Message);
+                }
             }
             catch (Exception ex)//łapiemy wyjątka ;)
             {
