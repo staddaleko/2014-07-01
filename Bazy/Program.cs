2ee218322ac2cf18dcd268199b4592a4de2575f4
staddaleko <staddaleko@gmail.com>
2014-07-01 14:10:02 +0200

    pierwsze dzisiaj

---
+++ Bazy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = @"Data Source=(local)\SQLEXPRESS;Initial Catalog=Ksiazki;Integrated Security=True;";
+        }
+    }
+}
